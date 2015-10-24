72a97d04f9c01d5b32ba33ffbe06d4c4c0d2b93d
Eugeniy Sinelshchikov <e.sinleshchikov@readyforsky.com>
2015-10-24 19:07:55 +0300

    wip2

--- src/parkingplace.cpp
+++ src/parkingplace.cpp
@@ -4,3 +4,18 @@ ParkingPlace::ParkingPlace(QRectF rect)
     : AbstractPlace(rect, TYPE_PARKINGPLACE)
 {}
 
+QVector<AbstractPlace *> ParkingPlace::roads() const
+{
+    return m_roads;
+}
+
+void ParkingPlace::pushRoad(AbstractPlace *road)
+{
+    m_roads << road;
+}
+
+void ParkingPlace::setRoads(const QVector<AbstractPlace *> &roads)
+{
+    m_roads = roads;
+}
+
